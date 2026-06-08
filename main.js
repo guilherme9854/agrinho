7b6c4f0fb53dfbc52b1582534ad0518e6b45bce2
guilherme9854 <167765418+guilherme9854@users.noreply.github.com>
2026-06-08 12:20:01 -0300

    s

--- main.js
+++ main.js
@@ -1,42 +1,15 @@
-const themeBtn =
-document.getElementById("theme-btn");
-
-themeBtn.addEventListener("click",()=>{
-
-document.body.classList.toggle("dark");
-
-if(document.body.classList.contains("dark")){
-themeBtn.innerHTML="☀️";
-}else{
-themeBtn.innerHTML="🌙";
-}
-
-});
-
-const tabs =
-document.querySelectorAll(".tab-btn");
-
-const contents =
-document.querySelectorAll(".tab-content");
-
-tabs.forEach(tab=>{
-
-tab.addEventListener("click",()=>{
-
-tabs.forEach(btn=>{
-btn.classList.remove("active");
-});
-
-contents.forEach(content=>{
-content.classList.remove("active");
-});
-
-tab.classList.add("active");
-
-document
-.getElementById(tab.dataset.tab)
-.classList.add("active");
-
-});
-
+/* main.js */
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
 });
